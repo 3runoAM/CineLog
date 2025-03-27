7cf08271b316896dab43d1ee249625fb404b4f47
Bruno Martins <brunomartins@ads.fiponline.edu.br>
2025-03-27 17:16:59 -0300

    Exclui espaços desnecessários

--- src/main/java/com/brunoam/CineLog/repositories/MovieRepository.java
+++ src/main/java/com/brunoam/CineLog/repositories/MovieRepository.java
@@ -4,4 +4,5 @@ import com.brunoam.CineLog.entities.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+    //
 }
