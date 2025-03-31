97d08447dbdc76a9cdaa71b57a3bba2d8b2643df
Bruno Martins <brunomartins@ads.fiponline.edu.br>
2025-03-31 16:51:49 -0300

    Clean CinelogApplication class, add a summary option for the search movie DTO and changes methods in the tmdb client

--- src/main/java/com/brunoam/CineLog/CineLogApplication.java
+++ src/main/java/com/brunoam/CineLog/CineLogApplication.java
@@ -1,38 +1,15 @@
 package com.brunoam.CineLog;
 
-
-import com.brunoam.CineLog.services.client.MovieApiClient;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
-@RestController("/")
+@RequestMapping("/")
 public class CineLogApplication {
-	private final MovieApiClient tmdbClient;
-
-	public CineLogApplication(MovieApiClient tmdbClient) {
-		this.tmdbClient = tmdbClient;
-	}
 	public static void main(String[] args) {
 		SpringApplication.run(CineLogApplication.class, args);
-	}
-
-	@Bean
-	public CommandLineRunner run() {
-		return args -> {
-			tmdbClient.getMovieDetails(550) // Fight Club
-					.subscribe(movie -> System.out.println("Filme encontrado: " + movie.getTitle()));
-
-//			// Teste de busca por query
-//			tmdbClient.searchMovies("Matrix")
-//					.subscribe(movies -> movies.forEach(movie ->
-//							System.out.println("Resultado da busca: " + movie)));
-		};
 	}
 
 	@GetMapping
--- src/main/java/com/brunoam/CineLog/services/client/MovieApiClient.java
+++ src/main/java/com/brunoam/CineLog/services/client/MovieApiClient.java
@@ -1,8 +1,7 @@
 package com.brunoam.CineLog.services.client;
 
+import com.brunoam.CineLog.dto.response.movie.MovieSearchSummary;
 import com.brunoam.CineLog.entities.Movie;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -11,7 +10,6 @@ import java.util.List;
 
 @Service
 public class MovieApiClient {
-
     private final WebClient tmdbClient;
 
     public MovieApiClient(WebClient tmdbClient) {
@@ -25,10 +23,11 @@ public class MovieApiClient {
                 .bodyToMono(Movie.class);
     }
 
-    public Mono<List<Movie>>  searchMovies(String query) {
+    public Mono<List<Movie>> searchMovies(String query) {
         return tmdbClient.get()
-                .uri("/search/movie?query={query}", query)
+                .uri("/search/movie?query=" + query)
                 .retrieve()
-                .bodyToMono(new ParameterizedTypeReference<List<Movie>>() {});
+                .bodyToMono(MovieSearchSummary.class)
+                .map(MovieSearchSummary::results);
     }
 }
