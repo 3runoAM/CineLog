a633d629a13e0d7002007cf3998d8c986b543f38
Bruno Martins <brunomartins@ads.fiponline.edu.br>
2025-04-01 18:25:56 -0300

    Mofify class User

--- src/main/java/com/brunoam/CineLog/entities/base/AuditableEntity.java
+++ src/main/java/com/brunoam/CineLog/entities/base/AuditableEntity.java
@@ -1,4 +1,20 @@
 package com.brunoam.CineLog.entities.base;
 
-public class AuditableEntity {
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+public abstract class AuditableEntity {
+    @Column(nullable = false, updatable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 }
