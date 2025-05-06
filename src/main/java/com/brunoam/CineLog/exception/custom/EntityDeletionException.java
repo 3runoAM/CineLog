4ae1484261ebfcef76ef9d4926e9ffc621b6a1b2
Bruno Martins <brunomartins@ads.fiponline.edu.br>
2025-05-06 18:51:22 -0300

    EntityDeletionException now extends RuntimeException instead of Exception

--- src/main/java/com/brunoam/CineLog/exception/custom/EntityDeletionException.java
+++ src/main/java/com/brunoam/CineLog/exception/custom/EntityDeletionException.java
@@ -1,6 +1,6 @@
 package com.brunoam.CineLog.exception.custom;
 
-public class EntityDeletionException extends Exception {
+public class EntityDeletionException extends RuntimeException {
     public EntityDeletionException(String message) {
         super(message);
     }
